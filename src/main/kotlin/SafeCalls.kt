ce2b85baeb2d4a58a1588932040ce958095399a9
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-09-09 20:27:39 +0700

    Add SafeCalls.kt

---
+++ src/main/kotlin/SafeCalls.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+//    Safe calling
+//    ?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+
+//    The ?. chaining
+//    Student?. teacher?.supervisor?.name = "Seymour Skinner"
+}
