68a232e258e09c4854c457e2dc021e31305acb55
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-09-09 20:51:57 +0700

    Add AkarKuadrat.kt

---
+++ src/main/kotlin/AkarKuadrat.kt
@@ -0,0 +1,15 @@
+import kotlin.math.sqrt
+
+fun main(args: Array<String>) {
+//    calculate square root
+//    sqrt is an abbreviation of square root
+    print("Enter some number and I'll calculate a square root: ")
+    val a = readLine()!!.toDouble()
+    if (a > 0){
+        println("The number you entered is greater then 0, so I can calculate it!")
+        val root = sqrt(a);
+        println("The square root of $a is $root")
+    }else
+        println("I can't calculate the square root of negative number!")
+    println("Thanks for the input")
+}
